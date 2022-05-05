88c2a210ed0e9b0eede3cf61e87eceb7249ee530
Timo1024 <47331071+Timo1024@users.noreply.github.com>
2022-05-05 18:11:31 +0200

    added dark mode

--- assets/js/script_main.js
+++ assets/js/script_main.js
@@ -1,28 +1,25 @@
+initialize()
+
 window.onload = function() {
     
     // Dark-Light mode switch
     // 0 ist light mode
     // 1 ist dark  mode
 
-    new_color = localStorage.getItem("color_mode") == "1" ? "0" : "1";
-    localStorage.setItem("color_mode", new_color);
     switching_icon();
 
     let switch_mode = document.querySelector('#mode_switch');
     
     switch_mode.addEventListener("click", function() {
         switching();
+        switching_icon()
     });
 };
-
-new_color = localStorage.getItem("color_mode") == "1" ? "0" : "1";
-localStorage.setItem("color_mode", new_color);
-switching();
 
 function switching_icon() {
     let switch_mode_new = document.querySelector('#mode_switch');
     switch (localStorage.getItem("color_mode")) {
-        case "0":
+        case "1":
             switch_mode_new.innerHTML = "<svg class=\"bi\" id=\"sun_icon\" width=\"1em\" height=\"1em\"><use xlink:href=\"#sun\"/></svg>";
             break;
         default:
@@ -31,31 +28,60 @@ function switching_icon() {
     }
 }
 
+function initialize() {
+    
+    dark_mode = localStorage.getItem("color_mode") === "1" ? true : false;
+    
+    var fileref=document.createElement("link")
+    fileref.setAttribute("rel", "stylesheet")
+    fileref.setAttribute("type", "text/css")
+    fileref.setAttribute("href", "./assets/css/dark.css")
+    if(!dark_mode){fileref.setAttribute("disabled", "")}
+    document.getElementsByTagName("head")[0].appendChild(fileref)
+    
+    var fileref2=document.createElement("link")
+    fileref2.setAttribute("rel", "stylesheet")
+    fileref2.setAttribute("type", "text/css")
+    fileref2.setAttribute("href", "./assets/css/light.css")
+    if(dark_mode){fileref2.setAttribute("disabled", "")}
+    document.getElementsByTagName("head")[0].appendChild(fileref2)
+    
+}
+
 function switching() {
     console.log("von: " + localStorage.getItem("color_mode"));
-
-    // let switch_mode_new = document.querySelector('#mode_switch');
-    // let doc_body = document.querySelector('body');
-    // let text_in_header = document.querySelector('.fs-4');
-    // let menu_top = document.getElementsByClassName('nav-link');
 
     switch (localStorage.getItem("color_mode")) {
         case "0":
             // dark mode
             localStorage.setItem("color_mode", "1");
-            // switch_mode_new.innerHTML = "<svg class=\"bi\" id=\"sun_icon\" width=\"1em\" height=\"1em\"><use xlink:href=\"#sun\"/></svg>";
-            // doc_body.classList.add("dark");
-            // text_in_header.classList.add("dark");
-            // for (var i of menu_top){i.classList.add("dark")};
+
+            styles = document.getElementsByTagName("link")
+
+            for(var i of styles){
+                if(i.href.endsWith("light.css")){
+                    i.disabled = true;
+                }
+                if(i.href.endsWith("dark.css")){
+                    i.disabled = false;
+                }
+            }
 
             break;
         default:
             // light mode
             localStorage.setItem("color_mode", "0");
-            // switch_mode_new.innerHTML = "<svg class=\"bi\" id=\"moon_icon\" width=\"1em\" height=\"1em\"><use xlink:href=\"#moon\"/></svg>";
-            // doc_body.classList.remove("dark");
-            // text_in_header.classList.remove("dark");
-            // for (var i of menu_top){i.classList.remove("dark")};
+
+            styles = document.getElementsByTagName("link")
+            
+            for(var i of styles){
+                if(i.href.endsWith("light.css")){
+                    i.disabled = false;
+                }
+                if(i.href.endsWith("dark.css")){
+                    i.disabled = true;
+                }
+            }
 
             break;
     }
